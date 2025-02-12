0e1d6da8b4d9e456ffafc0a170226e8cf2caa221
Ashwin kumar singh <kaushik.ashwin2626@gmail.com>
2025-02-12 23:39:33 +0530

    project setup for Chat Application

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className="font-bold text-purple-600">This is a Chat Application</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
